b7268ee8ba3326d7e59c51e795111bb5a0e9a817
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-03-02 23:46:52 +0600

    setup a backend for lifelinemeds

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+// import globalErrorHander from './app/middleware/globalErrorHander';
+// import notFound from './app/middleware/notFound';
+// import router from './app/routes';
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors({ origin: ['https://assigement-04-front-end.vercel.app'],credentials:true }));
+app.use(cookieParser());
+
+// application routes
+app.use('/api/v1', router);
+
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello World!');
+};
+
+app.get('/', getAController);
+
+app.use(globalErrorHander);
+
+
+
+// Not Found
+app.use(notFound);
+
+export default app;
